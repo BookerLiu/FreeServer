a8f28a8b088bf7ca2dc18cf2082f5fd9edc7b47f
Demo-Liu <demoliu94@163.com>
2019-04-15 13:39:52 +0800

    添加三丰云参数

--- postpone/src/main/java/com/ff/postpone/task/util/ParamUtil.java
+++ postpone/src/main/java/com/ff/postpone/task/util/ParamUtil.java
@@ -9,34 +9,12 @@ public class ParamUtil {
 
 
     public static String SINA="";
+    //阿贝云
     public static String ABEI_TITLE = "谈一谈近期使用阿贝云服务器的感受";
     public static String ABEI_BODY = "总的来说,阿贝云的免费服务器,拥有5M带宽,这在免费当中真的是良心满满了,此外,阿贝云还有2G2H5M、15元/月 4G4H5M、25元/月 优惠云服务器 可以购买,很实惠的,真的是很棒!!!";
-
+    //三丰云
     public static String SANFENG_TITLE = "谈一谈近期使用三丰云服务器的感受";
     public static String SANFENG_BODY = "总的来说,三丰云的免费服务器,拥有5M带宽,这在免费当中真的是良心满满了,此外,三丰云还有2G2H5M、15元/月 4G4H5M、25元/月 优惠云服务器 可以购买,很实惠的,真的是很棒!!!地址:https://www.sanfengyun.com";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     //阿贝云
