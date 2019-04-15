f501f8430bff17a9bcc41074779c328fb4076598
Demo-Liu <demoliu94@163.com>
2019-04-15 13:37:03 +0800

    修改数据库

--- postpone/src/main/resources/database.sql
+++ postpone/src/main/resources/database.sql
@@ -7,9 +7,10 @@ MySQL - 5.6.36 : Database - abeiyun
 数据库很简单   只存储了一些用户信息
  */
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`abeiyun` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `abeiyun`;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`freeserver` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `freeserver`;
 
 /*Table structure for table `user_info` */
 
@@ -25,6 +26,13 @@ CREATE TABLE `user_info` (
   `next_time` datetime DEFAULT NULL COMMENT '下次延期时间',
   `sina_url` varchar(255) DEFAULT 'success' COMMENT '延期博客url',
   `status` varchar(2) DEFAULT '1' COMMENT '0 使用本地cookie, 1 使用接口获取cookie',
+  `bz` varchar(2) DEFAULT NULL COMMENT '0 阿贝云, 1 三丰云, 2 同时启用   (是否同时申请了阿贝云和三丰云的服务器)',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
