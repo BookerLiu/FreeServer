03c9f26781ebfffae355820a626f8ad92084d2d7
Demo-Liu <demoliu94@163.com>
2019-06-14 17:21:23 +0800

    重构项目

--- postpone/src/main/java/com/ff/postpone/task/CommCode.java
+++ postpone/src/main/java/com/ff/postpone/task/CommCode.java
@@ -2,71 +2,27 @@ package com.ff.postpone.task;
 
 import com.ff.postpone.mapper.UserInfoMapper;
 import com.ff.postpone.pojo.UserInfo;
-import com.ff.postpone.task.util.HttpUtil;
-import com.ff.postpone.task.util.ParamUtil;
+import com.ff.postpone.task.util.Sina;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-import org.apache.commons.httpclient.Cookie;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
 /**
- * @Author Demo-Liu
- * @Date 2019/3/19 10:50
+ * @Author LiuFei
+ * @Date 2019/6/13 16:55
  * @description
  */
 public class CommCode {
 
 
     private static Logger log = Logger.getLogger(CommCode.class);
-
-
-    /**
-     * 获取云 url
-     * @param bz 判断是哪个云服务器
-     * @param logic 0 登录, 1 业务操作
-     * @return
-     */
-    public static String getYunUrl(int bz,int logic){
-        switch (bz){
-            case 0:
-                if(logic==0){
-                    return ParamUtil.ABEI_LOGIN;
-                }else{
-                    return ParamUtil.ABEI_LOGIC;
-                }
-            case 1:
-                if(logic==0){
-                    return ParamUtil.SANFENG_LOGIN;
-                }else{
-                    return ParamUtil.SANFENG_LOGIC;
-                }
-        }
-        return null;
-    }
-
-    /**
-     * 获取云服务器名字
-     * @param bz
-     * @return
-     */
-    public static String getYunName(int bz){
-        switch(bz){
-            case 0:
-                return "阿贝云";
-            case 1:
-                return "三丰云";
-        }
-        return null;
-    }
-
     /**
      * 检查服务器状态
      * @return
@@ -107,8 +63,8 @@ public class CommCode {
      * @param infoMapper
      * @return
      */
-    public static int checkCheckStatus(JSONObject json, UserInfo userInfo, UserInfoMapper infoMapper, List<String> cookieList) throws IOException {
-        String username = userInfo.getYunusername();
+    public static int checkCheckStatus(JSONObject json, UserInfo userInfo, UserInfoMapper infoMapper, List<String> cookieList) throws IOException, URISyntaxException {
+        String username = userInfo.getCloudUser();
         json = JSONObject.fromObject(json.getString("msg"));
         JSONArray array = JSONArray.fromObject(json.getString("content"));
         if(array.size()>0){
@@ -120,19 +76,19 @@ public class CommCode {
                 return 1;
             }else if("审核通过".equals(state)){
                 log.info(username + "审核通过!!!");
-                if(userInfo.getSinaUrl().equals(url)){
-                    userInfo.setSinaUrl("success");
+                if(userInfo.getBlogUrl().equals(url)){
+                    userInfo.setBlogUrl("success");
                     infoMapper.updateByPrimaryKey(userInfo);
-                    deleteBlog(userInfo,url,infoMapper,cookieList);
+                    Sina.deleteBlog(userInfo,url,infoMapper,cookieList);
                     log.info("修改url为success,删除延期博客!!!");
                 }
                 return 2;
             }else{
                 log.info(username + "审核失败,审核结果:"+state);
-                if(userInfo.getSinaUrl().equals(url)){
-                    userInfo.setSinaUrl("error");
+                if(userInfo.getBlogUrl().equals(url)){
+                    userInfo.setBlogUrl("error");
                     infoMapper.updateByPrimaryKey(userInfo);
-                    deleteBlog(userInfo,url,infoMapper,cookieList);
+                    Sina.deleteBlog(userInfo,url,infoMapper,cookieList);
                     log.info("修改url为error,删除延期博客!!!");
                 }
                 return 3;
@@ -144,250 +100,10 @@ public class CommCode {
     }
 
     /**
-     * 删除延期博客
-     * @param info
-     * @param url
-     * @param infoMapper
+     * 根据博客类型调用不同的方法删除博客
+     * @param userInfo
      */
-    public static void deleteBlog(UserInfo info, String url, UserInfoMapper infoMapper, List<String> cookieList) throws IOException {
-        log.info("开始删除"+url);
-        String[] strs = url.split("_");
-        String[] params = strs[1].split(".html");
-        NameValuePair delete[] = {
-                new NameValuePair("blog_id[]",params[0]),
-                new NameValuePair("uid","5118414786")
-        };
-        HttpClient sinaClient = HttpUtil.getHttpClient("UTF-8");
-        String deleteUrl = "http://control.blog.sina.com.cn/admin/article/article_del_recycle.php?domain=1";
+    private static void deleteBlogByType(UserInfo userInfo){
 
-        Map<String,String> map;
-        if("1".equals(info.getStatus())){ //从接口获取cookie
-            log.info("开始接口获取sinaCookie");
-            if(cookieList.get(0)==null){
-                log.info("开始第一次获取cookie....");
-               cookieList.set(0,getSinaCookie(info));
-            }
-            map = CommCode.getPubHeader(cookieList.get(0));
-        }else{ //从本地获取cookie
-            cookieList.set(0,info.getSinaCookie());
-            map = CommCode.getPubHeader(cookieList.get(0));
-        }
-        map.put("Referer","http://blog.sina.com.cn/s/blog_"+params[0]+".html");
-        String postRes = HttpUtil.getPostRes(sinaClient, deleteUrl, delete, map);
-        log.info("删除博客返回:"+postRes);
-        String[] postRess;
-        JSONObject json = null;
-        if(postRes.length()>10){
-            postRess = postRes.split("</script>");
-            json = JSONObject.fromObject(postRess[1]);
-        }
-        try{
-            if(json!=null){
-                if("A00006".equals(json.getString("code"))){
-                    log.info(url+"删除成功!!!");
-                    infoMapper.updateByPrimaryKey(info);
-                    completelyDelete(sinaClient, params[0], cookieList.get(0));
-                }else{
-                    log.info(url+"删除失败!!!");
-                }
-
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 从回收站删除博客
-     * @param sinaClient
-     * @param param
-     * @param cookie
-     * @throws IOException
-     */
-    public static void completelyDelete(HttpClient sinaClient, String param, String cookie) throws IOException {
-        log.info("调用彻底删除接口...");
-        String url = "http://control.blog.sina.com.cn/admin/article/article_recycle_del.php";
-        GetMethod getMethod = new GetMethod(url);
-        NameValuePair delete[] = {
-                new NameValuePair("blog_id[]",param),
-                new NameValuePair("uid","5118414786"),
-                new NameValuePair("varname","requestId_40385686")
-        };
-        Map<String,String> map = getPubHeader(cookie);
-        getMethod.setQueryString(delete);
-        String getRes = HttpUtil.getGetRes(sinaClient, url, delete, map);
-        log.info("彻底删除博文返回:"+ getRes);
-    }
-
-
-
-    /**
-     * 获取新浪微博单点登录cookie
-     * @param info
-     * @return
-     * @throws IOException
-     */
-    public static String getSinaCookie(UserInfo info) throws IOException {
-        HttpClient httpClient = HttpUtil.getHttpClient("UTF-8");
-        String url = "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.15)&_=";
-        NameValuePair[] pairs = CommCode.getSinaLogin(info.getSinausername(),info.getSinapassword());
-        HttpUtil.getPostRes(httpClient,url,pairs,CommCode.getPubHeader(""));
-        String cookieStr = "";
-        Cookie[] cookies = httpClient.getState().getCookies();
-        for (Cookie cookie : cookies) {
-            cookieStr += cookie.toString()+";";
-        }
-        return cookieStr;
-    }
-
-
-
-    /**
-     * 获取公共请求头Map
-     * @param cookie
-     * @return
-     */
-    public static Map<String,String> getPubHeader(String cookie){
-        Map<String,String> map = new HashMap<>();
-        map.put("Referer", "https://weibo.com/");
-        map.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36");
-        map.put("Content-Type", "application/x-www-form-urlencoded");
-        map.put("Accept", "text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8");
-        map.put("Accept-Encoding", "deflate, br");
-        map.put("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
-        map.put("cookie",cookie);
-        return map;
-    }
-
-    /**
-     * 获取阿贝云登录参数
-     * @param username
-     * @param password
-     * @return
-     */
-    public static NameValuePair[] getYunLogin(String username, String password){
-        NameValuePair yunLogin[] = {
-                new NameValuePair("cmd","login"),
-                new NameValuePair("id_mobile",username),
-                new NameValuePair("password",password)
-        };
-        return yunLogin;
-    }
-
-    /**
-     * 获取查询服务器状态参数
-     * @return
-     */
-    public static NameValuePair[] getFreeStatus(){
-        NameValuePair freeDelay[] = {
-                new NameValuePair("cmd","check_free_delay"),
-                new NameValuePair("ptype","vps")
-        };
-        return  freeDelay;
-    }
-
-    /**
-     * 获取审核状态参数
-     * @return
-     */
-    public static NameValuePair[] getCheckStatus(){
-        NameValuePair status[] = {
-                new NameValuePair("cmd","free_delay_list"),
-                new NameValuePair("ptype","vps"),
-                new NameValuePair("count","20"),
-                new NameValuePair("page","1")
-        };
-        return status;
-    }
-
-    /**
-     * 获取发送微博参数
-     * @param bz
-     * @return
-     */
-    public static NameValuePair[] getSendBlog(int bz){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        String format = sdf.format(new Date());
-        String[] strs = format.split(" ");
-        NameValuePair blog[] = {
-                new NameValuePair("ptype",""),
-                new NameValuePair("teams",""),
-                new NameValuePair("worldcuptags",""),
-                new NameValuePair("album",""),
-                new NameValuePair("album_cite",""),
-                new NameValuePair("blog_id",""),
-                new NameValuePair("is_album","0"),
-                new NameValuePair("old365","0"),
-                new NameValuePair("stag",""),
-                new NameValuePair("sno",""),
-                new NameValuePair("book_worksid",""),
-                new NameValuePair("channel_id",""),
-                new NameValuePair("url",""),
-                new NameValuePair("channel",""),
-                new NameValuePair("newsid",""),
-                new NameValuePair("fromuid",""),
-                new NameValuePair("wid",""),
-                new NameValuePair("articletj",""),
-                new NameValuePair("vtoken","4fcebcbd1a24c119fd14998b8c9c635d"),
-                new NameValuePair("is_media","0"),
-                new NameValuePair("is_stock","0"),
-                new NameValuePair("is_tpl","0"),
-                new NameValuePair("assoc_article",""),
-                new NameValuePair("assoc_style","0"),
-                new NameValuePair("assoc_article_data",""),
-                new NameValuePair("article_BGM",""),
-                new NameValuePair("xRankStatus",""),
-                new NameValuePair("commentGlobalSwitch",""),
-                new NameValuePair("commenthideGlobalSwitch",""),
-                new NameValuePair("articleStatus_preview","1"),
-                new NameValuePair("source",""),
-                new NameValuePair("topic_id","0"),
-                new NameValuePair("topic_channel","0"),
-                new NameValuePair("topic_more",""),
-                new NameValuePair("utf8","1"),
-                //new NameValuePair("conlen","96"),
-                new NameValuePair("date_pub",strs[0]),
-                new NameValuePair("time",strs[1]),
-                new NameValuePair("new_time",""),
-                new NameValuePair("isTimed","0"),
-                new NameValuePair("immediatepub","0"),
-                new NameValuePair("blog_title", bz==0 ? ParamUtil.ABEI_TITLE + (int)(Math.random()*100000000) : ParamUtil.SANFENG_TITLE + (int)(Math.random()*100000000)),
-                new NameValuePair("blog_body", bz==0 ?ParamUtil.ABEI_BODY : ParamUtil.SANFENG_BODY),
-                new NameValuePair("blog_class","00"),
-                new NameValuePair("tag","it"),
-                new NameValuePair("x_cms_flag","2"),
-                new NameValuePair("x_quote_flag","1"),
-                new NameValuePair("sina_sort_id","117")
-        };
-        return blog;
-    }
-
-
-    /**
-     * 获取新浪微博登录参数
-     * @param username
-     * @param password
-     * @return
-     */
-    public static NameValuePair[] getSinaLogin(String username, String password){
-        NameValuePair[] pairs = {
-                new NameValuePair("entry", "sso"),
-                new NameValuePair("gateway", "1"),
-                new NameValuePair("from", "null"),
-                new NameValuePair("savestate", "30"),
-                new NameValuePair("useticket", "0"),
-                new NameValuePair("pagerefer", ""),
-                new NameValuePair("vsnf", "1"),
-                new NameValuePair("su", Base64.getEncoder().encodeToString(username.getBytes())),
-                new NameValuePair("service", "sso"),
-                new NameValuePair("sp", password),
-                new NameValuePair("sr", "1024*768"),
-                new NameValuePair("encoding", "UTF-8"),
-                new NameValuePair("cdult", "3"),
-                new NameValuePair("domain", "sina.com.cn"),
-                new NameValuePair("prelt", "0"),
-                new NameValuePair("returntype", "TEXT")
-        };
-        return pairs;
     }
 }
--- postpone/src/main/java/com/ff/postpone/task/Postpone.java
+++ postpone/src/main/java/com/ff/postpone/task/Postpone.java
@@ -4,13 +4,13 @@ import com.ff.postpone.mapper.UserInfoMapper;
 import com.ff.postpone.pojo.UserInfo;
 import com.ff.postpone.pojo.UserInfoExample;
 import com.ff.postpone.task.util.HttpUtil;
+import com.ff.postpone.task.util.ParamUtil;
+import com.ff.postpone.task.util.Sina;
+import com.ff.postpone.task.util.UrlUtil;
 import net.sf.json.JSONObject;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.multipart.FilePart;
-import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
-import org.apache.commons.httpclient.methods.multipart.Part;
-import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -40,14 +41,14 @@ public class Postpone {
     public void postpone(){
         //查询所有云账号
         UserInfoExample example = new UserInfoExample();
-        List<UserInfo> userInfos = infoMapper.selectByExampleWithBLOBs(example);
+        List<UserInfo> userInfos = infoMapper.selectByExample(example);
 
         List<String> cookieList = new ArrayList<>();
         cookieList.add(null);
         try{
             //根据不同服务器获取不同url进行操作
             for (UserInfo userInfo : userInfos) {
-                String bz = userInfo.getBz();
+                String bz = userInfo.getCloudType();
                 switch (bz){
                     case "0": //阿贝云
                         logic(userInfo,0,cookieList);
@@ -65,19 +66,19 @@ public class Postpone {
 
     public void logic(UserInfo userInfo, int bz, List<String> cookieList) throws Exception{
         JSONObject json;
-        String username = userInfo.getYunusername();
-        log.info(username + " 开始登录"+CommCode.getYunName(bz)+"!!!");
-        HttpClient yunClient = HttpUtil.getHttpClient("UTF-8");
-        json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient,  CommCode.getYunUrl(bz,0), CommCode.getYunLogin(username, userInfo.getYunpassword())));
-        log.info("登录"+CommCode.getYunName(bz)+"返回:"+json.toString());
+        String username = userInfo.getCloudUser();
+        log.info(username + " 开始登录"+ ParamUtil.getYunName(bz)+"!!!");
+        HttpClient yunClient = HttpUtil.getHttpClient();
+        json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient,  ParamUtil.getYunUrl(bz,0), ParamUtil.getYunLogin(username, userInfo.getCloudPass())));
+        log.info("登录"+ ParamUtil.getYunName(bz)+"返回:"+json.toString());
         String loginMsg = json.getString("msg");
         if("登录成功".equals(loginMsg)){
-            log.info(username + CommCode.getYunName(bz)+"登录成功,开始检查免费服务器状态!!!");
-            json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient, CommCode.getYunUrl(bz,1), CommCode.getFreeStatus()));
+            log.info(username + ParamUtil.getYunName(bz)+"登录成功,开始检查免费服务器状态!!!");
+            json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient, ParamUtil.getYunUrl(bz,1), ParamUtil.getFreeStatus()));
             log.info("检查服务器状态返回:"+json.toString());
             int status = CommCode.checkServerStatus(json, username, infoMapper, userInfo);
             log.info("服务器状态:"+status);
-            json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient, CommCode.getYunUrl(bz,1), CommCode.getCheckStatus()));
+            json = JSONObject.fromObject(HttpUtil.getPostRes(yunClient, ParamUtil.getYunUrl(bz,1), ParamUtil.getCheckStatus()));
             log.info("检查审核状态返回:"+json);
             switch (status){
                 case 1:  //已到审核期
@@ -99,27 +100,25 @@ public class Postpone {
      * @param info
      * @param yunClient
      * @param infoMapper
-     * @throws Exception
      */
     public void sentBlog(UserInfo info, HttpClient yunClient, UserInfoMapper infoMapper, int bz, List<String> cookieList) throws Exception {
         log.info("开始发送延期博客...");
-        String url = "http://control.blog.sina.com.cn/admin/article/article_post.php";
 
         Map<String,String> map;
-        if("1".equals(info.getStatus())){ //从接口获取cookie
+        if("1".equals(info.getCookieType())){ //从接口获取cookie
             log.info("开始接口获取sinaCookie");
             if(cookieList.get(0)==null){
                 log.info("开始第一次获取cookie....");
-                cookieList.set(0,CommCode.getSinaCookie(info));
+                cookieList.set(0, Sina.getSinaCookie(info));
             }
-            map = CommCode.getPubHeader(cookieList.get(0));
+            map = ParamUtil.getPubHeader(cookieList.get(0));
         }else{ //从本地获取cookie
-            map = CommCode.getPubHeader(info.getSinaCookie());
+            map = ParamUtil.getPubHeader(info.getBlogCookie());
         }
         map.put("Referer","http://control.blog.sina.com.cn/admin/article/article_add.php");
         log.info("sina cookie:"+map.get("cookie"));
-        HttpClient sinaClient = HttpUtil.getHttpClient("UTF-8");
-        String postRes = HttpUtil.getPostRes(sinaClient, url, CommCode.getSendBlog(bz), map);
+        HttpClient sinaClient = HttpUtil.getHttpClient();
+        String postRes = HttpUtil.getPostRes(sinaClient, UrlUtil.SINA_SEND, ParamUtil.getSendBlog(bz), map);
 
         log.info("延期博客返回结果:"+postRes);
         JSONObject json = JSONObject.fromObject(postRes);
@@ -127,8 +126,8 @@ public class Postpone {
         String code = json.getString("code");
         if("B06001".equals(code) && data!=null && !"null".equals(data)){
             String sinaUrl = "http://blog.sina.com.cn/s/blog_" + data + ".html";
-            log.info("更新"+info.getYunusername()+" url为:"+sinaUrl);
-            info.setSinaUrl(sinaUrl);
+            log.info("更新"+info.getCloudUser()+" url为:"+sinaUrl);
+            info.setBlogUrl(sinaUrl);
             infoMapper.updateByPrimaryKey(info);
             if(createPic(sinaUrl)){
                 File file = new File("/usr/local/phantomjs-2.1.1/postponepic/postpone.png");
@@ -156,30 +155,42 @@ public class Postpone {
      * @param yunClient
      * @param url
      * @param file
-     * @throws Exception
      */
     public void sendAbei(UserInfo info, HttpClient yunClient, String url, File file, List<String> cookieList, int bz) throws Exception {
         log.info("开始提交延期记录!!!");
-        PostMethod postMethod = new PostMethod(CommCode.getYunUrl(bz,1));
-        FilePart filePart;
+//        HttpPost httpPost = new HttpPost(CommCode.getYunUrl(bz,1));
+        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+
         //三丰云和阿贝云文件提交方式不一样
-        if(bz==0){
-            //阿贝云
-            filePart = new FilePart("yanqi_img",file);
+        if(bz==0) {
+            multipartEntityBuilder.addBinaryBody("yanqi_img",file);
         }else{
-            //三丰云
-            filePart = new FilePart("yanqi_img","postpone.png",file,"image/png","UTF-8");
+            multipartEntityBuilder.addBinaryBody("yanqi_img",file, ContentType.IMAGE_PNG,"postpone.png");
         }
-        filePart.setCharSet("utf-8");
-        Part[] parts = {
-                new StringPart("cmd","free_delay_add" , "utf-8"),
-                new StringPart("ptype","vps" , "utf-8"),
-                new StringPart("url",url , "utf-8"),
-                filePart};
-        MultipartRequestEntity entity = new MultipartRequestEntity(parts, postMethod.getParams());
-        postMethod.setRequestEntity(entity);
-        yunClient.executeMethod(postMethod);
-        String postRes = postMethod.getResponseBodyAsString();
+        multipartEntityBuilder.setCharset(Charset.forName("UTF-8"));
+        ContentType contentType = ContentType.create("text/plain", Charset.forName("UTF-8"));
+        multipartEntityBuilder.addTextBody("cmd","free_delay_add" ,contentType);
+        multipartEntityBuilder.addTextBody("ptype","vps" ,contentType);
+        multipartEntityBuilder.addTextBody("url","url" ,contentType);
+        String postRes = HttpUtil.getPostRes(yunClient, ParamUtil.getYunUrl(bz,1), multipartEntityBuilder.build(), ParamUtil.getPubHeader(""));
+
+        //三丰云和阿贝云文件提交方式不一样
+//        if(bz==0){
+//            //阿贝云
+//            filePart = new FilePart("yanqi_img",file);
+//        }else{
+//            //三丰云
+//            filePart = new FilePart("yanqi_img","postpone.png",file,"image/png","UTF-8");
+//        }
+//        filePart.setCharSet("utf-8");
+//        Part[] parts = {
+//                new StringPart("cmd","free_delay_add" , "utf-8"),
+//                new StringPart("ptype","vps" , "utf-8"),
+//                new StringPart("url",url , "utf-8"),
+//                filePart};
+//        MultipartRequestEntity entity = new MultipartRequestEntity(parts, httpPost.getParams());
+//        yunClient.executeMethod(httpPost);
+//        String postRes = httpPost.getResponseBodyAsString();
         log.info("提交延期记录返回结果:"+postRes);
         try{
             JSONObject json = JSONObject.fromObject(postRes);
@@ -187,13 +198,14 @@ public class Postpone {
                 log.info("提交延期记录成功!!!");
             }else{
                 log.info("提交延期记录失败,删除发布博客!!!");
-                CommCode.deleteBlog(info,url,infoMapper,cookieList);
+                Sina.deleteBlog(info,url,infoMapper,cookieList);
             }
         }catch (Exception e){
             log.info("提交延期记录失败,删除发布博客!!!");
-            CommCode.deleteBlog(info,url,infoMapper,cookieList);
+            Sina.deleteBlog(info,url,infoMapper,cookieList);
         }
         file.delete();
+
     }
 
     /**
