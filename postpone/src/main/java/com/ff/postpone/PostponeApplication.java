11729954a6315732f684a527e8c0d8c362527ffd
Demo-Liu <demoliu94@163.com>
2020-08-24 16:39:26 +0800

    删除无用信息

--- postpone/src/main/java/com/ff/postpone/PostponeApplication.java
+++ postpone/src/main/java/com/ff/postpone/PostponeApplication.java
@@ -1,6 +1,5 @@
 package com.ff.postpone;
 
-import com.ff.postpone.constant.Profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ public class PostponeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostponeApplication.class, args);
-        System.out.println(Profile.MAIL_SERVER_HOST);
     }
 
 //    @Override
